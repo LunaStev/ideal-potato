3ac1fb99ccbf67e398bdae0eeb18976e2fd09c12
Quin0019 <96914208+Quin0019@users.noreply.github.com>
2022-02-01 12:12:39 +0900

    Update MessageBox.c

--- MessageBox.c
+++ MessageBox.c
@@ -1,7 +1,6 @@
 #include <windows.h>
 
 int APIENTRY WinMain(HINSTANCE hIns, HINSTANCE hPrev, LPSTR cmd, INT nShow) {
-    MessageBox(0,TEXT("TEST"), TEXT("Gurm IT Studios"), MB_OK);
+    MessageBox(0,TEXT(/*내용*/"TEST"), TEXT(/*Title*/"Gurm IT Studios"), MB_OK);
     return 0;
-    
 }
