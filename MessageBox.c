bcb3793f9f4df6ebba2685bcb447d95c38bd6acf
Quin0019 <96914208+Quin0019@users.noreply.github.com>
2022-02-01 12:09:31 +0900

    MessageBox

---
+++ MessageBox.c
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int APIENTRY WinMain(HINSTANCE hIns, HINSTANCE hPrev, LPSTR cmd, INT nShow) {
+    MessageBox(0,TEXT("TEST"), TEXT("Gurm IT Studios"), MB_OK);
+    return 0;
+}
